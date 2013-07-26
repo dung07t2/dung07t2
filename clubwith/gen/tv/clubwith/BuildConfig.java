2af5900d1f05390f50abb088f2c4813125dc38c9
dung07t2 <dung07t2@gmail.com>
2013-07-26 10:15:24 +0700

    STT

---
+++ clubwith/gen/tv/clubwith/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tv.clubwith;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ clubwith/src/tv/clubwith/ClubWithHome.java
@@ -0,0 +1,22 @@
+package tv.clubwith;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ClubWithHome extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_clubwith_home);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.club_with_home, menu);
+		return true;
+	}
+
+}
